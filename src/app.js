1bffbdbd65fbadeef299dc20c876c547ea2d7b73
Bino26 <hbino1999@gmail.com>
2023-11-21 01:46:00 +0000

    Fixed cors bug

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const connectToDb = require("./db/db");
-const cors = require("cors");
 const favicon = require("express-favicon");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 //connection to the database
 connectToDb();
@@ -12,10 +12,11 @@ connectToDb();
 const mainRouter = require("./routes/mainRouter.js");
 
 // middleware
-app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true })); //cross the app
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true })); //cross the app
 app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(favicon(__dirname + "/public/favicon.ico"));
