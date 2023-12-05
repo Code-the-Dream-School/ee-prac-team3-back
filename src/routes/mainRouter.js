efbdfe8f13178f4dde28aa49dacf78dd545f0c91
Bino26 <hbino1999@gmail.com>
2023-12-05 10:41:27 +0000

    Add Forgot and Reset Password

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { signUp, logIn, logOut } = require("../controllers/userController");
+const {
+  signUp,
+  logIn,
+  logOut,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/userController");
 const signupDataValidate = require("../middlewares/signupDataValidate");
 const loginDataValidate = require("../middlewares/loginDataValidate");
 const authenticateUser = require("../middlewares/authenticateUser");
@@ -9,5 +15,7 @@ const router = express.Router();
 router.post("/signup", signupDataValidate, signUp);
 router.route("/login").post(loginDataValidate, logIn).get(authenticateUser);
 router.get("/logout", logOut);
+router.post("/forgotpassword", forgotPassword);
+router.post("/resetpassword/:token", resetPassword);
 
 module.exports = router;
