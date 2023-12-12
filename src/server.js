7a4bf3a3c81e29d0fd015ded2bc230411900d11c
evaw277 <evaweisneck@gmail.com>
2023-12-12 14:51:57 -0500

    Update server.js for deployment
    Updated middleware for FE/BE connection on deployed site

--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
+// require("dotenv").config();
+// const app = require("./app");
+// const PORT = process.env.PORT || 8000;
+
+// const listener = () => console.log(`Listening on Port ${PORT}!`);
+// app.listen(PORT, listener);
+
 require("dotenv").config();
-const app = require("./app");
+const express = require("express");
+const cors = require("cors"); // Import the cors middleware
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
 const PORT = process.env.PORT || 8000;
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
