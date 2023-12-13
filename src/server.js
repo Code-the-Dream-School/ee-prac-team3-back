907c68d3e4d2836cd690c088defb3eaf80595f52
evaw277 <evaweisneck@gmail.com>
2023-12-12 20:12:13 -0500

    Print .env variables in console

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-console.log("test!!!!!!!!!!!");
+console.log("process.env.PORT ====> ", process.env.PORT);
+console.log("process.env.MONGO_URI ====> ", process.env.MONGO_URI);
 const app = require("./app");
 const PORT = process.env.PORT || 8000;
 
