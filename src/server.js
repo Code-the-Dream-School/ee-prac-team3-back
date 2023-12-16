c1e2c5299596e71e0ce79b3a029bcd5514a86015
evaw277 <evaweisneck@gmail.com>
2023-12-15 21:53:03 -0500

    secure flag test

--- src/server.js
+++ src/server.js
@@ -9,13 +9,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors"); // Import the cors middleware
 const app = express();
-
+const cookieParser = require("cookie-parser");
 // Enable CORS for all routes
 app.use(cors());
 
 //sameSite
 // app.use(cookieParser());
-// const cookieParser = require("cookie-parser");
+
 // app.get("/", (res) => {
 //   // Set a cookie with SameSite=None; Secure attributes
 //   res.cookie("exampleCookie", "exampleValue", {
@@ -27,28 +27,28 @@ app.use(cors());
 // });
 
 //set Access-Control-Allow-Credentials to true
-app.use((res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://cute-conkies-9d76b9.netlify.app/"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-//Secure flag is set on cookies. Sends cookies only over HTTPS
-// app.use(cookieParser());
-
-// app.get("/", (res) => {
-//   // Set a cookie with the Secure flag
-//   res.cookie("exampleCookie", "exampleValue", { secure: true, httpOnly: true });
-//   res.send("Cookie set with Secure flag");
+// app.use((res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://cute-conkies-9d76b9.netlify.app/"
+//   );
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
 // });
+
+// Secure flag is set on cookies. Sends cookies only over HTTPS
+app.use(cookieParser());
+
+app.get("/", (res) => {
+  // Set a cookie with the Secure flag
+  res.cookie("exampleCookie", "exampleValue", { secure: true, httpOnly: true });
+  res.send("Cookie set with Secure flag");
+});
 
 const PORT = process.env.PORT || 8000;
 
